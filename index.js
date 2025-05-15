10f79ad3ac2652bce7dd63e98095fc4ad2efbd0e
md-zeon <zeonrahaman5870@gmail.com>
2025-05-15 13:05:44 +0600

    add single coffee fetch functionality

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
             res.send(result);
         })
 
+        // get a single coffee
+        app.get('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
+
         // add a coffee
         app.post('/coffees', async (req, res) => {
             const newCoffee = req.body;
